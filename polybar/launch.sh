e7586a542652c8a8ba4361fdf73133cd0f6ba451
Nar Cuenca <narc.ph@gmail.com>
2021-03-15 23:52:50 +0800

    updated polybar theme and alacritty theme

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,7 +5,9 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
-# Launch bar1 and bar2
+# Launch polybar 
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar mainbar 2>&1 | tee -a /tmp/polybar1.log & disown
+
+polybar -c $HOME/.config/polybar/config.ini mainbar &
+
 echo "Bars launched..."
